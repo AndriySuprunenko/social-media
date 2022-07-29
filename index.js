a7a79b4920be875c02b8f385d8e016de58fdca12
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2022-07-29 17:59:44 +0300

    validation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import { registerValidation } from './validations/auth.js';
 import { validationResult } from 'express-validator';
@@ -8,7 +9,7 @@ import UserModel from './models/User.js';
 // Mongodb
 mongoose
   .connect(
-    'mongodb+srv://admin:admin@cluster0.cxibak0.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://admin:admin@cluster0.cxibak0.mongodb.net/blog?retryWrites=true&w=majority'
   )
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
@@ -19,14 +20,30 @@ const app = express();
 // читає json формат
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt); //шифруєм пароль
+
+    const doc = new UserModel({
+      email: req.body.email,
+      passwordHash,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+    });
+
+    const user = await doc.save();
+
+    res.json(user);
+  } catch (err) {
+    res.json(err);
   }
-  res.json({
-    success: true,
-  });
 });
 
 // порт серверу
